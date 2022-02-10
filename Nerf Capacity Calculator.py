29e2fae1836974dbf33975c26e9135f52e9b8c48
petkeeper18 <96896686+petkeeper18@users.noreply.github.com>
2022-02-10 11:40:34 -0800

    allow saving to text file

--- Nerf Capacity Calculator.py
+++ Nerf Capacity Calculator.py
@@ -3,11 +3,20 @@ different_mag = int(input("How many different magazine/speedloader sizes? >> "))
 total_ammo = int(internal_clip)
 mag = int(0)
 while mag < different_mag:
-    mag_capacity = int(input("Ammo capacity of magazine/speedloader size " + str(mag+1) + "? >> "))
-    ammo_storage = int(input("How many "+ str(mag_capacity) +" capacity magazine/speedloaders? >> "))
+    mag_capacity = int(input("Ammo capacity of magazine/speedloader size " + str(mag + 1) + "? >> "))
+    ammo_storage = int(input("How many " + str(mag_capacity) + " capacity magazine/speedloaders? >> "))
     total_ammo += mag_capacity * ammo_storage
     mag += 1
 print(str(total_ammo) + " ammo held")
 game_time = int(input("How many minutes is the game? >> "))
 ammo_per_minutes = total_ammo / game_time
 print(str(ammo_per_minutes) + " shots per minute")
+
+save_file = input("Save as text file? (Y/N) >>")
+if save_file == "Y" or save_file == "y":
+    save = open("Nerf Capacity.txt", "w")
+    with open('Nerf Capacity.txt', 'w') as save:
+        save.write(str(total_ammo) + " ammo held total\n")
+        save.write(str(ammo_per_minutes) + " shots per minute for a " + str(game_time) + " minute game.")
+else:
+    pass
