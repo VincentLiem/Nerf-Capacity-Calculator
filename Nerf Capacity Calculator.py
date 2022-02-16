794aaf9eb96322118352ec6d1cb4b604d7a5f36a
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-02-15 22:16:28 -0800

    Update Nerf Capacity Calculator.py

--- Nerf Capacity Calculator.py
+++ Nerf Capacity Calculator.py
@@ -1,3 +1,4 @@
+# Write your code here :-)
 internal_clip = input("Internal clip capacity? >> ")
 different_mag = int(input("How many different magazine/speedloader sizes? >> "))
 total_ammo = int(internal_clip)
@@ -9,13 +10,16 @@ while mag < different_mag:
     mag += 1
 print(str(total_ammo) + " ammo held")
 game_time = int(input("How many minutes is the game? >> "))
-ammo_per_minutes = total_ammo / game_time
-print(str(ammo_per_minutes) + " shots per minute")
-
+if game_time != 0:
+    ammo_per_minutes = total_ammo / game_time
+    print(str(ammo_per_minutes) + " shots per minute")
+else: 
+    print("No game time entered")
 save_file = input("Save as text file? (Y/N) >> ")
 if save_file == "Y" or save_file == "y":
     blaster_name = input("Blaster name >> ")
     with open(blaster_name + ' Capacity.txt', 'w') as save:
-        save.write(str(total_ammo) + " ammo held total\n")
+        save.write(blaster_name + '\n')
+        save.write(str(total_ammo) + ' ammo held total')
 else:
     pass
