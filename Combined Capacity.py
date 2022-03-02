838f1f0981176a2edf7753d543a6ef062e9c9d27
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-03-01 19:59:14 -0800

    Update Combined Capacity.py

--- Combined Capacity.py
+++ Combined Capacity.py
@@ -1,10 +1,11 @@
 import glob
 
 files = glob.glob('* Capacity.txt')
-with open('Combined Capacity.txt', 'w') as combined:
+result_file_name = 'Combined Capacity.txt'
+with open(result_file_name, 'w') as combined:
     for file in files:
         with open(file) as opened:
-            if file != 'Combined Capacity.txt':
+            if file != result_file_name:
                 contents = opened.read()
                 combined.write(contents +'\n')
         opened.close()
